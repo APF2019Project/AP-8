5914fa27eff81c9151d7383533ed1350b2f63061
jafar <jafarsdeghi07@gmail.com>
2019-11-28 21:33:39 +0330

    some transformation

--- src/model/Card.java
+++ src/model/Card.java
@@ -4,9 +4,21 @@ public class Card {
     private String name ;
     private CardType cardType;
 
-    public Card(String name, CardType cardType) {
+    public Coin getShopingPrice() {
+        return shopingPrice;
+    }
+
+    public void setShopingPrice(Coin shopingPrice) {
+        this.shopingPrice = shopingPrice;
+    }
+
+    private Coin shopingPrice;
+
+
+    public Card(String name, CardType cardType, Coin shopingPrice) {
         this.name = name;
         this.cardType = cardType;
+        this.shopingPrice = shopingPrice;
     }
 
     public String getName() {
--- src/model/Plant.java
+++ src/model/Plant.java
@@ -1,13 +1,21 @@
 package model;
 
-public class Plant {
-    private int health ;
+public class Plant extends Card {
+    private int health = 100;
     private PlantType plantType;
+    private Turn coolDownIncreasePerTurn;
     private Turn cooLDown;
-    private Turn coolDownCeil ;
+    private Turn coolDownCeil;
     private Sun plantingPrice;
-    private  Coin shopingPrice;
-    private  Turn sunOutTurn;
+    private Coin shopingPrice;
+    private Turn sunOutTurn;
+    private Home position;
 
+    private Condition condition = Condition.LIVE;
+
+    public void setCondition() {
+        if(this.health<=0)
+            this.condition=Condition.DEAD;
+    }
 
 }
