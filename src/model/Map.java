95dda081001becd15d93f9b30b5b490876f6eff7
dibamsh <diba.masihi@gmail.com>
2019-12-14 23:31:01 +0330

    secound commit

---
+++ src/model/Map.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Map {
+    private int [][] matrixOfCells;
+    private ArrayList<Cell> landCells = new ArrayList<Cell>();
+    private ArrayList<Cell> wateerCells = new ArrayList<Cell>();
+    public void  (){
+
+    }
+}
