cc13bd9bfce10cbcbe793af798ebc1b2f0a7e375
jafar <jafarsdeghi07@gmail.com>
2019-12-16 00:11:40 +0330

    start doing handeling plants

---
+++ src/model/Account.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Account {
+    // list of  zombies and plants most be declare
+    private String name;
+    private String username;
+    private String password;
+    private String coins; //we have two modle coin one is this , that is usefull for shoping , other is in game type
+    private int numberOfKiledZombies = 0;
+    private Collection Collection;
+
+
+}
---
+++ src/model/Menu.java
@@ -0,0 +1,7 @@
+package model;
+
+import java.util.HashMap;
+
+public class Menu {
+    public HashMap<String , Account> accounts = new HashMap<>(); //save accounts with password
+}
