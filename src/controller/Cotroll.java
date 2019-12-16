9421bedcd1e357ca6a1e8552992ae1454fe6e48a
dibamsh <diba.masihi@gmail.com>
2019-12-16 11:32:27 +0330

    commit

--- src/controller/Cotroll.java
+++ src/controller/Cotroll.java
@@ -1,4 +1,7 @@
 package controller;
 
 public class Cotroll {
+// man class cell ro zadam age chizi b nazaret bayad ezafe koni ezafe kon va inke b nazaram aslan zaroorati b class map nadrim tarjihan pak she
+// .plant ha faqa mitunan toye khune haye zoj bashan ino lotfan dar class plant piyade sazi kon k hatman moqei k put plant mikonim toyemazrabe 2 put she.
 }
+
--- src/model/Cell.java
+++ src/model/Cell.java
@@ -1,5 +1,39 @@
 package model;
 
 public class Cell {
+    private double x;
+    private double y;
+    private CellType cellType;
 
+    public Cell() {
+    }
+
+    public Cell(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public CellType getCellType() {
+        return cellType;
+    }
+
+    public void setCellType(CellType cellType) {
+        this.cellType = cellType;
+    }
 }
