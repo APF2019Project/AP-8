54d3ad600712694d5c7b54104afae965003de00e
jafar <jafarsdeghi07@gmail.com>
2019-12-31 21:38:03 +0330

    LeaderBoard handled

--- src/model/repository/LeaderBoard.java
+++ src/model/repository/LeaderBoard.java
@@ -1,5 +1,9 @@
 package model.repository;
 
+import model.entity.Account;
+
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class LeaderBoard {
@@ -10,7 +14,19 @@ public class LeaderBoard {
     }
     public static void leaderBoardTakeInput(){
         System.out.println("----------LEADER BOARD-------------");
-        System.out.println("enter \"back\" to back loggin menu");
-
+        System.out.println("enter \"back\" to back loggin menu //another case will show leader board");
+        String enr = input.nextLine();
+        switch (enr){
+            case "back":
+                LogginMenu.getInputForLogginMenu();
+                break;
+            default:
+                ArrayList<Account> temp = Account.getAccounts();
+                temp.sort(Comparator.comparingInt(Account::getNumberOfKiledZombies));
+                for (Account account: temp) {
+                    System.out.println(account.getName()+ ": " +account.getNumberOfKiledZombies());
+                }
+                LogginMenu.getInputForLogginMenu();
+        }
     }
 }
