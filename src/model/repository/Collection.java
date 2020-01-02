af0e1df55760f2bf187d9a970e140da91dccb403
jafar <jafarsdeghi07@gmail.com>
2020-01-02 16:31:53 +0330

    impplementing Collection

--- src/model/repository/Collection.java
+++ src/model/repository/Collection.java
@@ -3,10 +3,11 @@ package model.repository;
 import model.entity.Account;
 import model.entity.Card;
 import model.entity.CovertCardsToJsonString;
+import model.exeptions.InvalidBulletTypeExeption;
+import model.exeptions.InvalidPlantTypeExeption;
 import model.exeptions.InvalidZombieTypeExeption;
 
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -14,7 +15,7 @@ public class Collection {
     private Card type;
     private int maxSize =7;
     private static Scanner input;
-    private HashMap<String, Card> selectedCards = new HashMap<>(7);
+    private static HashMap<String, Card> selectedCards = new HashMap<>(7);
 
     public Card getType() {
         return type;
@@ -33,29 +34,84 @@ public class Collection {
     }
     public static HashMap<String , Card> takeInputForCollection(String cardType){
         System.out.println("---------COLLECTION_MENU---------");
-        System.out.println("enter"+cardType + "names you want to use in game:");
-        System.out.println("enter end to go play");
-        if(cardType.matches("zombie")){
-            return takeInputForZombie();
-        }else if(cardType.matches("plant")){
-            return takeInputForPlant();
+        System.out.println("enter one of below commands" +
+                "showHand" +
+                "play" +
+                "select cardName" +
+                "remove cardName" +
+                "showCollection");
+        String str= input.nextLine();
+        String[] splites = str.split(" ");
+        switch (splites[0]){
+            case "showHand":
+                showSelectedCards();
+                takeInputForCollection(cardType);
+                break;
+            case "showCollection" :
+                showUnselectedCards(cardType);
+                takeInputForCollection(cardType);
+                break;
+            case "play" :
+                return Collection.selectedCards;
+            case "select" :
+                select(splites[1] , cardType);
+                takeInputForCollection(cardType);
+                break;
+            case "remove":
+                remove(splites[1]);
+                takeInputForCollection(cardType);
+                break;
+            default: takeInputForCollection(cardType);
+        }
+        return Collection.selectedCards;
+    }
+
+    private static void select(String cardName, String cardType) {
+
+       if(cardType.matches("zombie")){
+           if(Account.loggedInAccount.getZombies().contains(cardName)){
+               try {
+                   selectedCards.put(cardName , CovertCardsToJsonString.getZombeiFromJsonString(cardName));
+               } catch (InvalidZombieTypeExeption | FileNotFoundException invalidZombieTypeExeption) {
+                   invalidZombieTypeExeption.printStackTrace();
+               }
+           }
+       }else if(cardType.matches("plant")){
+           if(Account.loggedInAccount.getPlants().contains(cardName)){
+               try {
+                   selectedCards.put(cardName , CovertCardsToJsonString.getPlantFromJsonString(cardName));
+               } catch (InvalidPlantTypeExeption | InvalidBulletTypeExeption | FileNotFoundException invalidPlantTypeExeption) {
+                   invalidPlantTypeExeption.printStackTrace();
+               }
+           }
+       }
+    }
+    public static void remove(String cardName){
+        selectedCards.remove(cardName);
+    }
+    private static void showUnselectedCards(String cardType) {
+        switch (cardType){
+            case "zombie":
+                for (String name : Account.loggedInAccount.getZombies()) {
+                    if(!selectedCards.containsKey(name)){
+                        System.out.println(selectedCards.get(name).getName());
+                    }
+                }
+                break;
+            case "plant":
+                for (String name : Account.loggedInAccount.getPlants()) {
+                    if(!selectedCards.containsKey(name)){
+                        System.out.println(selectedCards.get(name).getName());
+                    }
+                }
         }
     }
 
-    private static HashMap<String, Card> takeInputForZombie() {
-        String str = input.nextLine();
-        switch (str){
-            case "end":
-                return Collection.
+    private static void showSelectedCards() {
+        for (String name : Collection.selectedCards.keySet()) {
+            System.out.println(Collection.selectedCards.get(name).getName());
         }
     }
 
-//    public static boolean selectZombie(String name){
-//        if(Account.loggedInAccount.getZombies().contains(name)){
-//            return true;
-//        }else {
-//            return false;
-//        }
-//    }
 
 }
